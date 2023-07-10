8fd10847a392aeb7e1e77809bf2f825a1945946b
PraneethKumar_Gangireddy <praneethkumarsmiles@gmail.com>
2023-07-10 11:06:16 -0500

    Added Internet Gateway,Routes tables and associations

--- main.tf
+++ main.tf
@@ -28,3 +28,33 @@ resource "aws_subnet" "VPC1_subnets" {
   }
   
 }
+
+resource "aws_internet_gateway" "internet_gateway" {
+  count = length(aws_vpc.VPC)
+  vpc_id = aws_vpc.VPC[count.index].id
+  tags = {
+    Name = "Internet_Gateway_for_VPC${count.index+1}"
+  }
+  
+}
+
+resource "aws_route_table" "route_table" {
+  count =length(aws_vpc.VPC)
+  vpc_id = aws_vpc.VPC[count.index].id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway[count.index].id
+  }
+  tags = {
+    Name = "VPC${count.index+1}routetable"
+  }
+
+  
+}
+
+resource "aws_route_table_association" "routetable_association" {
+  count = length(aws_subnet.VPC1_subnets)
+  subnet_id = aws_subnet.VPC1_subnets[count.index].id
+  route_table_id = aws_route_table.route_table[0].id
+  
+}
--- variables.tf
+++ variables.tf
@@ -10,6 +10,26 @@ variable "subnet_cidr_block" {
 
   
 }
+# variable "subnet_cidr_block_VPC2" {
+#     description = "CIDR blocks for VPCs"
+#     default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24","10.0.4.0/24"]
+
+  
+# }
+
+variable "amis" {
+    description = "Image_ids"
+    default = {
+        us-east-1= "ami-06ca3ca175f37dd66"
+        us-east-2= "ami-069d73f3235b535bd"
+    }
+  
+}
+variable "env" {
+    description = "Environment"
+    default = "PROD"
+  
+}
 
 
 
